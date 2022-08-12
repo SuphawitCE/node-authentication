21702b7ae4fbc50b61798efcfd0247544a50ddb0
SuphawitCE <b4nknaja@hotmail.com>
2022-08-12 13:00:12 +0700

    feat(send-mail): Feature send mail for user sign-up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,20 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+
+// const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  host: 'gmail',
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -14,6 +28,7 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    // isAuthenticated: false
     errorMessage: message
   });
 };
@@ -72,6 +87,8 @@ exports.postLogin = (req, res, next) => {
 
 exports.postSignup = (req, res, next) => {
   // Should have a validation for incoming request
+  console.log('post-signup-given: ', req.body);
+
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
@@ -95,7 +112,19 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           console.log('post-signup-response: ', result);
+
           res.redirect('/login');
+          // Sending mail options
+          return transporter.sendMail({
+            from: `Bank pmlo-qa <${process.env.EMAIL_USERNAME}>`,
+            to: email,
+            subject: 'Sending mail from Node.js application',
+            text: `User ${email} has confirm signup`,
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch((error) => {
+          console.log('send-mail-error: ', error);
         });
     })
     .catch((error) => {
