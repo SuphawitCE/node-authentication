f807037afdc74718f846e363dedaf810a5883e9a
SuphawitCE <b4nknaja@hotmail.com>
2022-08-13 18:00:35 +0700

    feat(validation): Feature validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -26,6 +28,7 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    // isAuthenticated: false
     errorMessage: message
   });
 };
@@ -42,13 +45,29 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    },
+    validationErrors: []
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   User.findOne({ email })
     .then((user) => {
@@ -88,46 +107,66 @@ exports.postSignup = (req, res, next) => {
 
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
 
-  User.findOne({ email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash('error', 'Email exists already, please try a different one.');
-        return res.redirect('/signup');
-      }
+  const errors = validationResult(req);
 
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then((result) => {
-          console.log('post-signup-response: ', result);
+  if (!errors.isEmpty()) {
+    console.log('errors: ', errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email,
+        password,
+        confirmPassword: req.body.confirmPassword
+      },
+      validationErrors: errors.array()
+    });
+  }
 
-          res.redirect('/login');
-          // Sending mail options
-          return transporter.sendMail({
-            from: `Bank pmlo-qa <${process.env.EMAIL_USERNAME}>`,
-            to: email,
-            subject: 'Sending mail from Node.js application',
-            text: `User ${email} has confirm signup`,
-            html: '<h1>You successfully signed up!</h1>'
-          });
-        })
-        .catch((error) => {
-          console.log('send-mail-error: ', error);
-        });
+  // User.findOne({ email })
+  //   .then((userDoc) => {
+  //     if (userDoc) {
+  //       req.flash('error', 'Email exists already, please try a different one.');
+  //       return res.redirect('/signup');
+  //     }
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      // if (password === confirmPassword) return user.save();
+
+      return user.save();
+
+      // throw new Error('Incorrect password');
+    })
+    .then((result) => {
+      console.log('post-signup-response: ', result);
+
+      res.redirect('/login');
+      // Sending mail options
+      return transporter.sendMail({
+        from: `Bank pmlo-qa <${process.env.EMAIL_USERNAME}>`,
+        to: email,
+        subject: 'Sending mail from Node.js application',
+        text: `User ${email} has confirm signup`,
+        html: '<h1>You successfully signed up!</h1>'
+      });
     })
     .catch((error) => {
-      console.log('post-signup-error: ', error);
-      throw error;
+      console.log('send-mail-error: ', error);
     });
+
+  // .catch((error) => {
+  //   console.log('post-signup-error: ', error);
+  //   throw error;
+  // });
 };
 
 exports.postLogout = (req, res, next) => {
@@ -135,4 +174,122 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect('/');
   });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  const email = req.body.email;
+
+  crypto.randomBytes(32, (error, buffer) => {
+    if (error) {
+      console.log(error);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email })
+      .then((user) => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        // Send email
+        res.redirect('/');
+
+        // return ?
+        transporter.sendMail({
+          from: `Bank pmlo-qa <${process.env.EMAIL_USERNAME}>`,
+          to: email,
+          subject: 'Password Reset',
+          text: `User ${email} has confirm signup`,
+          html: `
+          <h1>Reset password</h1>
+          <p>You requested a password reset</p>
+          <p>
+            Click this 
+            <a href="http://localhost:3000/reset/${token}" > link </a> 
+            to set a new password 
+          </p>
+          `
+        });
+      })
+      .catch((error) => {
+        console.log('post-reset-password-error: ', error);
+      });
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() }
+  })
+    .then((user) => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'Reset Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      console.log('Reset password confirmed');
+      res.redirect('/login');
+    })
+    .catch((error) => {
+      console.log('post-new-password-error: ', error);
+    });
 };
